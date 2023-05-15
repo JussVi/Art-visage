9841c892ee2f30d4188225e1d0c91661941e65ab
JussVi <osadze_denis_007@mail.ru>
2023-05-15 19:50:39 +0600

    Layout

--- src/pages/main-page/index.jsx
+++ src/pages/main-page/index.jsx
@@ -11,7 +11,7 @@ const MainPage = () => {
           <p className='main-page-text'>Добро пожаловать в Студия Арт Визаж!</p>
           <button className='main-page-button'>Записаться на курс</button>
           <div className='main-block-info'>
-            <p className="main-block-info-people">10 000+</p>
+            <p className="main-block-info-people">30 000+</p>
             <p className="main-block-info-specialists">выпущенных специалистов</p>
             <p className='main-block-info-flat-stick'>|</p>
             <p className='main-block-info-years'>22</p>
@@ -20,8 +20,35 @@ const MainPage = () => {
         </div>
         <div className="main-page-img-wrap">
           <span className='main-page-img'></span>
-          <img src="images/" alt="" />
         </div>
+      </div>
+
+      <div className="why-we-wrap">
+        <p className="why-we-header">ПОЧЕМУ МЫ?</p>
+        <p className="why-we-underline"></p>
+        <div className="why-we-card-block">
+          <div className="why-we-card-wrap">
+            <div className="why-we-icon-wrap">
+              <span className="why-we-card-icon-diplom"></span>
+            </div>
+            <p className="why-we-card-text">Диплом международного образца</p>
+          </div>
+          
+          <div className="why-we-card-wrap">
+            <div className="why-we-icon-wrap">
+              <span className="why-we-card-icon-cut"></span>
+            </div>
+            <p className="why-we-card-text">Современное оборудование и программное обеспечение</p>
+          </div>
+
+          <div className="why-we-card-wrap">
+            <div className="why-we-icon-wrap">
+              <span className="why-we-card-icon-student"></span>
+            </div>
+            <p className="why-we-card-text">Индивидуальный подход к каждому студенту</p>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
