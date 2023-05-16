4510404fff9f7120598eebb9d26add0bf3d81bdb
JussVi <osadze_denis_007@mail.ru>
2023-05-16 16:19:52 +0600

    Added header and footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,8 +3,25 @@ import './style.css'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='footer-wrap'>
+      <span className="footer-underline"></span>
+      <div className="container">
+        <div className="footer-map-wrap">
+          <span className="footer-logo"></span>
+        </div>
+        <span className="footer-underline"></span>
+        <div className="footer-nav-wrap">
+          <nav className="footer-nav-menu">
+            <a href="" className="footer-nav-menu-item">Главная</a>
+            <a href="" className="footer-nav-menu-item">Курсы</a>
+            <a href="" className="footer-nav-menu-item">О нас</a>
+            <a href="" className="footer-nav-menu-item">Мастеры</a>
+            <a href="" className="footer-nav-menu-item">Галерея</a>
+            <a href="" className="footer-nav-menu-item">Контакты</a>
+          </nav>
+          <span className="footer-co">© 2023 Студия Арт Визаж</span>
+        </div>
+      </div>
     </div>
   );
 };
